9dc651e19f97bb6874dad3dcd9dcc5b5602bf05a
Alexandre Guenot <aguenot75@gmail.com>
2024-09-06 18:29:55 +0200

    Basic of login Front

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Login.css'
+function Login() {
+  return (
+        <div className='wrapper'>
+            <div className="title">
+                Welcome to Hogwarts
+            </div>
+            <div className="login-container">
+                <div className="login-credentials-container">
+                    <input className="login-credentials" type="text" placeholder='Username'/>
+                    <input className="login-credentials" type="password" placeholder='Password'/>
+                </div>
+                <input className="login-submit" type="submit" value="Login"/>
+                
+            </div>
+        </div>     
+  )
+}
+
+export default Login
